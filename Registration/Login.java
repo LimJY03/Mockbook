2fa391a31774389556a805a4791b5dc440574fcf
Lim JY <limjunyi03@gmail.com>
2023-06-11 05:01:33 +0000

    Edited folder name

--- Registration/Login.java
+++ Registration/Login.java
@@ -1,7 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * @author HuSSon
  */
+
+package Registration;
+
 import com.mycompany.project.DB.*;
 import java.awt.AWTException;
 import java.sql.Connection;
@@ -9,15 +11,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
-/**
- *
- * @author HuSSon
- */
-public class Login extends TraceBack{
-    private String username,password,emailAddress;
-    
-    public TraceBack Main()  throws InterruptedException, AWTException
-    {
+
+public class Login extends TraceBack {
+
+    private String username, password, emailAddress;
+
+    public TraceBack Main() throws InterruptedException, AWTException {
+        
         Scanner i = new Scanner(System.in);
         System.out.println("****************");
         System.out.println("*   MockBook   *");
@@ -25,18 +25,17 @@ public class Login extends TraceBack{
         System.out.println("\t\t\t\t\t-------------------------------------\t\t");
         System.out.println("\t\t\t\t\t|             Login Page            |");
         System.out.println("\t\t\t\t\t-------------------------------------\t\t");
-        System.out.println("\n\t\t This is The Login Page! You Will be Presented With Mulitiple Options you May Choose From Them!");
+        System.out.println(
+                "\n\t\t This is The Login Page! You Will be Presented With Mulitiple Options you May Choose From Them!");
 
         System.out.println("\n\t\t\t\t\t-------------------------------------\t\t");
         System.out.println("Type 1 to Login Using Your Username\nType 2 to Login Using Your Email\nType 0 To Go Back!");
         System.out.println("\t\t\t\t\t-------------------------------------\t\t");
         TraceBack returnedTraceBack = null;
-        
-        breaker: while(true)
-        {
+
+        breaker: while (true) {
             String getInt = i.nextLine();
-            switch(getInt)
-            {
+            switch (getInt) {
                 case "1":
                     Thread.sleep(500);
                     clearConsole();
@@ -69,17 +68,15 @@ public class Login extends TraceBack{
                     break;
             }
         }
-        
+
         return returnedTraceBack;
     }
-    
-    public String getUser()
-    {
+
+    public String getUser() {
         return this.username;
     }
-    
-    public boolean signInUsingUsername()
-    {
+
+    public boolean signInUsingUsername() {
         String name, pass;
         DBFactory db = new DBFactory();
         Scanner sc = new Scanner(System.in);
@@ -90,26 +87,25 @@ public class Login extends TraceBack{
         System.out.println("\t\t\t\t\t|        Login Using Username       |");
         System.out.println("\t\t\t\t\t-------------------------------------\t\t");
         System.out.println("\n");
-        do{
+        do {
             System.out.print("Enter your Username: ");
             name = sc.nextLine();
             System.out.print("Enter your Password: ");
             pass = sc.nextLine();
             System.out.println("");
-            if(CheckerForAccount(name,pass,"username"))
+            if (CheckerForAccount(name, pass, "username"))
                 break;
             System.out.println("Wrong Username/Password, Try Again!");
-        }while(true);
-        
+        } while (true);
+
         this.username = name;
         this.password = pass;
         System.out.println("You have Logged in Successfully!");
-        
+
         return true;
     }
-    
-    public boolean signInUsingEmail()
-    {
+
+    public boolean signInUsingEmail() {
         String email, pass;
         DBFactory db = new DBFactory();
         Scanner sc = new Scanner(System.in);
@@ -120,44 +116,42 @@ public class Login extends TraceBack{
         System.out.println("\t\t\t\t\t|          Login Using Email        |");
         System.out.println("\t\t\t\t\t-------------------------------------\t\t");
         System.out.println("\n");
-        do{
-        System.out.print("Enter your Email: ");
-        email = sc.nextLine();
-        System.out.print("Enter your Password: ");
-        pass = sc.nextLine();
-        System.out.println("");
-        if(CheckerForAccount(email,pass,"email"))
-            break;
+        do {
+            System.out.print("Enter your Email: ");
+            email = sc.nextLine();
+            System.out.print("Enter your Password: ");
+            pass = sc.nextLine();
+            System.out.println("");
+            if (CheckerForAccount(email, pass, "email"))
+                break;
             System.out.println("Wrong Email/Password, Try Again!");
-        }while(true);
-        
+        } while (true);
+
         this.emailAddress = email;
         this.password = pass;
         System.out.println("You have Logged in Successfully!");
         return true;
     }
-    
-    public boolean CheckerForAccount(String user, String password , String type)
-    {
-        if(type.equalsIgnoreCase("username"))
+
+    public boolean CheckerForAccount(String user, String password, String type) {
+        if (type.equalsIgnoreCase("username"))
             type = "Username";
-        else if(type.equalsIgnoreCase("email"))
+        else if (type.equalsIgnoreCase("email"))
             type = "Email";
-        else{
+        else {
             System.out.println("give propa type");
         }
         Connection connection = MyDataBase.establishConnection();
-        try{
+        try {
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM User WHERE " + type +" ='"+ user +"' AND Password = '" + password+ "'");
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT * FROM User WHERE " + type + " ='" + user + "' AND Password = '" + password + "'");
 
             if (rs.next()) {
                 return true;
             }
-            
-        }
-        catch(SQLException e)
-        {
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return false;
