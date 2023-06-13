508bf1212e8d1aa7de991dd92281f2b409d6fa23
DanialZ2705 <133082015+DanialZ2705@users.noreply.github.com>
2023-06-13 21:05:07 +0800

    Update RegularUser.java
    Integrating my User.java into RegularUser.java

--- AssignmentMerging/src/AccessControl/RegularUser.java
+++ AssignmentMerging/src/AccessControl/RegularUser.java
@@ -1,18 +1,17 @@
 package AccessControl;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 
 public class RegularUser extends User {
 
-    private String contact, gender, email, job, hobbies, address;
-    private int age, connection;
+    private String contact, gender, email, job, hobbies, address, name;
+    private int age, connection, id, numberOfFriends;
     private LocalDate birthday;
     private ArrayList<String> connection1 = new ArrayList<>();
     private ArrayList<String> connection2 = new ArrayList<>();
     private ArrayList<String> connection3plus = new ArrayList<>();
-    
-    
 
     public RegularUser(String username, String email, String contact, int age, String gender, String p) {
         super(username, p);
@@ -35,8 +34,13 @@ public class RegularUser extends User {
     	this(username, email, contact, age, gender, p,job,hobbies,address);
     	this.birthday=birthday;
     }
-
     
+    public RegularUser(String username, String email, String contact, int age, String gender, String p, String job, String hobbies, String address, LocalDate birthday, int id, String name, int numberOfFriends) 
+    {
+    	this(username, email, contact, age, gender, p, job, hobbies, address, birthday);
+        this.id = id;
+        this.name = name;
+    }
 
     public void setConnection(int connection) {
         this.connection = connection;
@@ -61,7 +65,14 @@ public class RegularUser extends User {
     public int getAge() {
         return this.age;
     }
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getJob() {
         return this.job;
     }
@@ -94,6 +105,28 @@ public class RegularUser extends User {
     	this.birthday = birthday;
     }
 
+    public int calculateAge() {
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthday, currentDate);
+        return period.getYears();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(){
+        this.name = name;
+    }
+    
+    public int getNumberOfFriends() {
+        return numberOfFriends;
+    }
+    
+    public void setNumberOfFriends(){
+        this.numberOfFriends = numberOfFriends;
+    }
+    
     public ArrayList<String> getConnection1() {
         return this.connection1;
     }
@@ -104,5 +137,21 @@ public class RegularUser extends User {
 
     public ArrayList<String> getConnection3plus() {
         return this.connection3plus;
+    }
+    
+    public void viewAccount(int connectionDegree) {
+        System.out.println("Name: " + getName());
+        System.out.println("Username: " + getUsername());
+        System.out.println("Email Address: " + getEmail());
+        System.out.println("Phone Number: " + getContact());
+        System.out.println("Birthday: " + getBirthday());
+        System.out.println("Age: " + calculateAge());
+        System.out.println("Address: " + getAddress());
+        System.out.println("Gender: " + getGender());
+        System.out.println("Number of Friends: " + getNumberOfFriends());
+        System.out.println("Hobbies: " + getHobbies());
+        System.out.println("Jobs: " + getJob());
+        System.out.println("Password: " + getPassword());
+        System.out.println("Connection Degree to the User: " + connectionDegree);
     }
 }
