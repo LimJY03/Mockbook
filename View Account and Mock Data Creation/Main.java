e5b7b30526b0a5b44cbcadb93b003cb9532f6f78
DanialZ2705 <133082015+DanialZ2705@users.noreply.github.com>
2023-06-13 20:59:09 +0800

    Update Main.java

--- View Account and Mock Data Creation/Main.java
+++ View Account and Mock Data Creation/Main.java
@@ -1,3 +1,5 @@
+package assignmentds;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.sql.Connection;
@@ -12,7 +14,7 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/mydatabase";
         String username = "root";
         String password = "Atsukomajitenshi.27";
-
+        int connectionDegree = 3;
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             FriendshipManager friendshipManager = new FriendshipManager(connection);
@@ -20,7 +22,7 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Choose an option:");
-            System.out.println("1. Create random friend connection");
+            System.out.println("1. Manage Friend connection");
             System.out.println("2. Create mock user");
             System.out.println("3. View account");
             System.out.print("Enter your choice: ");
@@ -29,11 +31,8 @@ public class Main {
             switch (choice) {
                 case 1:
                     try {
-                        // Create a random friend connection
-                        friendshipManager.createRandomFriendConnection();
-
-                        // Get mutual random friends
-                        friendshipManager.getMutualRandomFriends();
+                        // Prompt the user to choose to make friendship connection or viewing mutual friends
+                        friendshipManager.chooseFriendshipOption();
 
                         // Close the database connection
                         connection.close();
@@ -43,9 +42,11 @@ public class Main {
                     break;
                 case 2:
                     User mockUser = MockDataCreator.createMockUser();
-                    mockUser.viewAccount();
-                    MySQLStorage.saveUser(mockUser);
+                    mockUser.viewAccount(connectionDegree);
+                    MySQLStorage.saveUser((RegularUser) mockUser);
                     break;
+
+
                 case 3:
                     AccountFinder accountFinder = new AccountFinder();
                     accountFinder.findAccount();
@@ -54,8 +55,6 @@ public class Main {
                     System.out.println("Invalid choice");
             }
 
-            // Close the database connection
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
