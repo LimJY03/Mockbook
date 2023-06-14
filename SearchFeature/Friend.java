0db2a549a7dd25e569e4a2c9a3e2a080a5b273a4
Lim JY <limjunyi03@gmail.com>
2023-06-14 09:06:33 +0000

    Fixed public access

--- SearchFeature/Friend.java
+++ SearchFeature/Friend.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 
 public class Friend {
 	
-	public int connectionDegree;
-	public String username;
-	public ArrayList<String> mutualFriends = new ArrayList<>();
+	private int connectionDegree;
+	private String username;
+	private ArrayList<String> mutualFriends = new ArrayList<>();
 	
+	// Constructor
 	public Friend(String username, int connectionDegree) {
 		this.username = username;
 		this.connectionDegree = connectionDegree;
 	}
+
+	// Methods
+	public void setConnectionDegree(int connectionDegree) { this.connectionDegree = connectionDegree; }
+	public void setUsername(String username) { this.username = username; }
+	public void setMutualFriends(ArrayList<String> mutualFriends) { this.mutualFriends = mutualFriends; }
+	public void addMutualFriends(String friend) { this.mutualFriends.add(friend); }
+	public int getConnectionDegree() { return this.connectionDegree; }
+	public String getUsername() { return this.username; }
+	public ArrayList<String> getMutualFriends() { return this.mutualFriends; }
 }
