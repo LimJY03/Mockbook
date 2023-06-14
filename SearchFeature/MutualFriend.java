8024dd4a302d60d25b541568d40e835884333dff
Lim JY <limjunyi03@gmail.com>
2023-06-14 08:28:51 +0000

    Complete and clean up code

--- SearchFeature/MutualFriend.java
+++ SearchFeature/MutualFriend.java
@@ -10,32 +10,26 @@ public class MutualFriend {
 	public static ArrayList<String> getMutualFriends(RegularUser me, String targetFriendList) {
 	        
         ArrayList<String> mutualFriends = new ArrayList<>();
-
         PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> a.compareTo(b) > 0 ? 1 : -1);
 
         String[] targetFriends = {};
         
         if (targetFriendList != null) targetFriends = targetFriendList.split(",");
 
-        for (int i = 0; i < targetFriends.length - 1; i++) pq.offer(targetFriends[i]);
+        for (int i = 0; i < targetFriends.length; i++) pq.offer(targetFriends[i]);
 
-        String s1 = pq.poll();
+        int pointer = 0;
 
-        while (!pq.isEmpty()) {
-
-            String s2 = pq.poll();
-
-            if (s1.equals(s2)) {
-
-                if (!mutualFriends.contains(s1)) mutualFriends.add(s1);
-
-                s1 = pq.poll();
-                continue;
-            }
-
-            s1 = s2;
+        String s1, s2 = pq.poll();
+        
+        while (pointer < me.getConnection1().size() && s2 != null) {
+            
+            s1 = me.getConnection1().get(pointer);
+            
+            if (s1.equals(s2)) if (!mutualFriends.contains(s1)) mutualFriends.add(s1);
+            if (s1.compareTo(s2) <= 0) pointer++;       // [..., mutualfriend, ...] vs [mutualfriend, ...]
+            if (s1.compareTo(s2) >= 0) s2 = pq.poll();  // [mutualfriend, ...] vs [..., mutualfriend, ...]
         }
-
         return mutualFriends;
     }
 }
